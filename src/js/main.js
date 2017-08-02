e7f8794a67a140f25a96f80f662691f83862b50f
Andrew Bater <andrew.bater@wintechnologies.net>
2017-08-02 18:01:26 +0100

    Make unit follow mouse

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,27 @@ window.onload = function() {
     canvas = document.getElementById("GameCanvas");
     context = canvas.getContext("2d");
 
+    canvas.addEventListener("mousedown", handleMouseDown);
+
     testUnit = new unitClass();
     testUnit.reset();
 
     intervalId = setInterval(updateAll, 1000/30);
+}
+
+function handleMouseDown(evt) {
+    updateMousePosition(evt);
+}
+
+function updateMousePosition(evt) {
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+
+    testUnit.destX = mouseX;
+    testUnit.destY = mouseY;
 }
 
 function updateAll() {
@@ -19,8 +36,10 @@ function updateAll() {
 }
 
 function moveAll() {
+    testUnit.move();
 }
 
 function drawAll() {
     paintRect(0, 0, canvas.width, canvas.height, "black");
+    testUnit.draw();
 }
--- src/js/unit.js
+++ src/js/unit.js
@@ -1,17 +1,34 @@
 "use strict";
 
 const UNIT_RADIUS = 5;
+const UNIT_MOVE_SPEED = 2;
 
 function unitClass() {
     this.x = 0;
     this.y = 0;
+    this.destX = 0;
+    this.destY = 0;
 
     this.reset = function() {
         this.x = Math.random() * canvas.width/4;
         this.y = Math.random() * canvas.height/4;
+        this.destX = canvas.width/2;
+        this.destY = canvas.height/2;
     }
 
     this.move = function() {
+        if (this.x > this.destX) {
+            this.x -= UNIT_MOVE_SPEED;
+        }
+        if (this.x < this.destX) {
+            this.x += UNIT_MOVE_SPEED;
+        }
+        if (this.y > this.destY) {
+            this.y -= UNIT_MOVE_SPEED;
+        }
+        if (this.y < this.destY) {
+            this.y += UNIT_MOVE_SPEED;
+        }
     }
 
     this.draw = function() {
